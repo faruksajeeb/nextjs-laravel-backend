e072f083420686dfbc491b9430dbedff27c86e7f
faruksajeeb <ofsajeeb@gmail.com>
2025-10-05 19:06:23 +0600

    post management

---
+++ config/cors.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+// 'paths' => ['api/*', 'sanctum/csrf-cookie'],
+// 'allowed_methods' => ['*'],
+// 'allowed_origins' => ['http://localhost:8001'],
+// 'allowed_headers' => ['*'],
+// 'supports_credentials' => true, // if using Sanctum
+];
